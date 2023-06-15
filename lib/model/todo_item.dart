8109fd684add906d348e3ac2febceddc065c416e
lordRedsky <aminmuhammadrizki@gmail.com>
2023-06-15 14:12:34 +0800

    setup todoitem dan todo list page

---
+++ lib/model/todo_item.dart
@@ -0,0 +1,53 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class TodoItem {
+  final int? id;
+  final String title;
+  final String description;
+  final bool isDone;
+
+  TodoItem(this.id, this.title, this.description, this.isDone);
+  // final bool completed;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'description': description,
+      'isDone': isDone,
+    };
+  }
+
+  factory TodoItem.fromMap(Map<String, dynamic> map) {
+    return TodoItem(
+      map['id'] != null ? map['id'] as int : null,
+      map['title'] as String,
+      map['description'] as String,
+      map['isDone'] as bool,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory TodoItem.fromJson(String source) => TodoItem.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  TodoItem copyWith({
+    int? id,
+    String? title,
+    String? description,
+    bool? isDone,
+  }) {
+    return TodoItem(
+      id ?? this.id,
+      title ?? this.title,
+      description ?? this.description,
+      isDone ?? this.isDone,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'TodoItem(id: $id, title: $title, description: $description, isDone: $isDone)';
+  }
+}
---
+++ lib/pages/todo_list_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todolist_app/model/todo_item.dart';
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  List<TodoItem> todos = [];
+  bool isLoading = false;
+  int totalDone = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List App'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        width: size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Todo List',
+                  style: textTheme.bodyMedium,
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Sudah diselesaikan $totalDone',
+                    style: const TextStyle(
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Expanded(
+                    child: todos.isEmpty
+                        ? const Center(
+                            child: Text('tidak ada data'),
+                          )
+                        : const SizedBox(),
+                  )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
